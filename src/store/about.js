9801774919a5b89f0962dd4dc1b8b055d1ec13f3
cinnamon-crew <maestrocinna@gmail.com>
2024-07-17 15:27:08 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "HEROPY",
+    email: "thesecon@gmail.com",
+    blog: "https://heropy.blog",
+    phone: "+82-10-123-5678",
+    image: "https://heropy.blog/css/images/logo.png",
+  }),
+};
